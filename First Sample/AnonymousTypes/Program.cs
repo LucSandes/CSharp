42e54006793ec45fa45f00e7b96d9eb5c3495abe
unknown <topdown@TD-222.topdown.local>
2019-09-24 15:32:44 -0300

    statements

---
+++ First Sample/AnonymousTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ
+
+            //para colocar valores nullable
+            int? i = null;
+            string s = null;
+            double? d = null;
+
+            var valores = new []{ new { idade = 18, nome = "Luccas" }, new { idade = 20, nome = "Amanda"}};
+
+            foreach (var item in valores)
+            {
+                Console.WriteLine("Idade: " + item.idade.ToString());
+                Console.WriteLine("Nome: " + item.nome.ToString());
+                Console.WriteLine("-----------------------------");
+            }
+            Console.ReadKey();
+        }   
+    }
+}
---
+++ First Sample/Operator/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Sintaxe
+            int a = 10;
+            int b = 2;
+            int c = 30;
+
+            //Adição
+            int add = a + b;
+
+            //Subtração
+            int subt = a - b;
+
+            //Multiplicação
+            int mult = a * b;
+
+            //Divisão
+            int div = a / b;
+
+            //Resto
+            int rest = a % b;
+
+            //Primários
+            //x++ (incremento) ou x-- (decremento)
+
+            //Comparativos
+            if (a > b)
+            {
+                //Console.WriteLine("a > b");
+            }
+            if (a < b)
+            {
+                //Console.WriteLine("a < b");
+            }
+            if (a >= b)// => expressão lambda
+            {
+                // Console.WriteLine("a >= b");
+            }
+            if (a <= b)
+            {
+                // Console.WriteLine("a <= b");
+            }
+            if (a == b)
+            {
+                //Console.WriteLine("a == b");
+            }
+            if (a != b)
+            {
+                //Console.WriteLine("a != b");
+            }
+
+            //Logicos
+            //XOR - logico exclusivo(um ou outro)
+            if (true ^ false)
+                //Console.WriteLine("True")
+
+                //Condicionais
+                //AND(duas condições verdadeiras)
+                if (a > b && true)
+                    // Console.WriteLine("true");
+
+                    //OR(uma condição ou outra verdadeira)
+                    if (false || true)
+                        // Console.WriteLine("True");
+                        #endregion
+
+           Console.Write("Valor 1: ");
+           int v1 = int.Parse(Console.ReadLine());
+           Console.Write("Valor 2: ");
+           int v2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Soma: " + Convert.ToString(v1 + v2));
+            Console.WriteLine("Subtração: " + (v1 - v2));
+            Console.WriteLine("Multiplicação: " + (v1 * v2));
+            Console.WriteLine("Divisão: " + (v1 / v2));
+            Console.WriteLine();
+
+            if (v1 > 0 && v2 > 0)
+            {
+                Console.WriteLine("Valores v1 e v2 são maiores que zero!");
+            }
+
+            if (v1 + v2 <= 0 || v1 - v2 <= 0 || v1 * v2 <= 0 || v1 / v2 <= 0)
+            {
+                Console.WriteLine("Uma ou mais operações posúi valor menor ou igual a zero!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
