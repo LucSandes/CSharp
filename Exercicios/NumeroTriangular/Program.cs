e5b6c7c681ee58f54aa90f7c9a6e306bab3a0f61
unknown <topdown@TD-222.topdown.local>
2019-09-26 12:55:56 -0300

    NumeroTriangular

--- Exercicios/NumeroTriangular/Program.cs
+++ Exercicios/NumeroTriangular/Program.cs
@@ -10,11 +10,11 @@ namespace NumeroTriangular
             Console.WriteLine("--- --- --- --- --- --- --- --- --- --- ---");
             int n, tr;
 
-            Console.Write("Digite o valor a ser calculado: ");
-            Console.WriteLine("--- --- --- --- --- --- --- --- --- --- ---");
+            Console.Write("Digite o valor a ser calculado: ");            
             n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--- --- --- --- --- --- --- --- --- --- ---");
 
-            tr = n(n + 1) / 2;
+            tr = n * (n + 1) / 2;
 
             Console.Write("O valor triangular é " + tr);
 
