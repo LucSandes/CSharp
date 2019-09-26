ab324f42d6a183b1585751ace1394367b76e960b
unknown <topdown@TD-222.topdown.local>
2019-09-26 17:02:35 -0300

    Divisores, MMC, MaximoDivisorComum

---
+++ Exercicios/Divisores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Divisores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dado um número qualquer, ache todos os seus divisores");
+            Console.WriteLine("--- --- --- --- --- --- --- --- --- --- ---");
+
+            int n, r;
+
+            Console.Write("Digite o número: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            r = n;
+
+            while (r != 0)
+            {
+                if (n % r == 0)
+                {
+                    Console.WriteLine(r);
+                }
+                r = r - 1;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
